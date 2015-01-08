6c7453ba73375343d75ba024027a225b6886ee1b
rasbabbo <rasbabbo@yahoo.com>
2015-01-07 23:47:43 -0800

    primechecker

--- script.js
+++ script.js
@@ -96,6 +96,24 @@ $(document).ready(function() {
 
 // paliChecker("radr");
 
+// //best primechecker yet!
+// function primeChecker (n) {
+//   if (n===1) {
+//     return false;
+//   } else if (n===2) {
+//      return true;
+//   } else {
+//     for (var x = 2; x < n; x++) {
+//       if(n % x === 0) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
+
+// console.log(primeChecker(44));
+
 
 
 
