39a1035317634783e5c4167c96da1cd7a27228b4
rasbabbo <rasbabbo@yahoo.com>
2015-01-07 23:08:00 -0800

    simple calc functions

--- script.js
+++ script.js
@@ -61,6 +61,22 @@ $(document).ready(function() {
 // }
 
 
+// //beginning of a calc - be good to use a prototype or constructor to simplify
+// function multiplyBy()
+// {
+//         num1 = document.getElementById("firstNumber").value;
+//         num2 = document.getElementById("secondNumber").value;
+//         document.getElementById("result").innerHTML = num1 * num2;
+// }
+
+// function divideBy() 
+// { 
+//         num1 = document.getElementById("firstNumber").value;
+//         num2 = document.getElementById("secondNumber").value;
+// document.getElementById("result").innerHTML = num1 / num2;
+// }
+
+
 
 
 
